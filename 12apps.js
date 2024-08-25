43dd1a1ad9048dbf2dc2d5da35ac0b366d88f842
vitavococo <gcastanedalondono@gmail.com>
2024-08-25 17:58:09 -0500

    clase 12 delegación de eventos

--- 12apps.js
+++ 12apps.js
@@ -1,13 +1,19 @@
-// const listItems = document.querySelectorAll("li");
+ /*const listItems = document.querySelectorAll("li");
 
-// listItems.forEach((item) => {
-//   item.addEventListener("click", (event) => {
-//     event.target.classList.toggle("highlight");
-//   });
-// });
+listItems.forEach((item) => {
+   item.addEventListener("click", (event) => {
+     event.target.classList.toggle("highlight");
+   });
+ });
+*/
+// Queremos que al agregar código y al señalar un elemento se active una clase que permita cambiarle  el color a un elemento.
+// Hay dos formas una es agregar un evento por cada option y la otra es el delegatio patern la responsabilidad queda sobre la etiqueta padre, que es la ul 
+
+// Si  cada elemento tiene un evento asociado el performance puede ser un poco más bajo.
 
 const list = document.querySelector("ul");
-
-list.addEventListener("click", (event) => {
-  event.target.closest("li").classList.toggle("highlight");
-});
+list.addEventListener("click",
+    (event) =>{
+        event.target.closest("li").
+        classList.toggle("highlight")
+    });
