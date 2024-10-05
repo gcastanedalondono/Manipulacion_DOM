732366866f2442db2eb89c3c7ee96d69d9bc56f6
vitavococo <gcastanedalondono@gmail.com>
2024-10-05 07:33:01 -0500

    ejercicio de la clase 5

--- 5apps.js
+++ 5apps.js
@@ -12,7 +12,20 @@ function cambiarEstilo() {
 
     var menu = document.querySelector(menu);
     menu.style.backgroundColor = "red";
+}
 
-    
+function volver() {
+    var title =document.getElementById("app-title");
+    title.style.color="black";
+    title.style.backgroundColor="white";
+
+    var title2=document.getElementById("second_title");
+    title2.innerText="con alegría saldreis";
+
+    var parrafito =document.getElementById("parrafo");
+    parrafito.style.textAlign = 'left';
+    parrafito.style.fontSize = '20px';
+
+
 
 }
