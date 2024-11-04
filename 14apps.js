d07b11ae7d331b35bb010aafcdd5509a6b11d7be
vitavococo <gcastanedalondono@gmail.com>
2024-11-04 12:17:26 -0500

    editando la sección de videos

---
+++ 14apps.js
@@ -0,0 +1,14 @@
+const container = document.querySelector(".container");
+const button = document.querySelector("button");
+
+container.addEventListener("mouseover", () =>{
+    container.style.backgroundColor="blue";
+});
+
+container.addEventListener("mouseout", () =>{
+    container.style.backgroundColor="red";
+});
+
+button.addEventListener("click", () =>{
+    alert("button clicked!");
+});
