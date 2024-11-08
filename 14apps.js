2fcc3bae823e3a6ccec03006d5db3cd4eca4cb3d
vitavococo <gcastanedalondono@gmail.com>
2024-11-08 07:45:50 -0500

    Manejando eventos con el mouseover

--- 14apps.js
+++ 14apps.js
@@ -1,14 +1,39 @@
 const container = document.querySelector(".container");
 const button = document.querySelector("button");
+const aparece = document.querySelector(".Najman");
+
+
 
 container.addEventListener("mouseover", () =>{
     container.style.backgroundColor="blue";
+   
 });
+
+
 
 container.addEventListener("mouseout", () =>{
     container.style.backgroundColor="red";
 });
 
-button.addEventListener("click", () =>{
-    alert("button clicked!");
+
+
+button.addEventListener("mouseover", () =>{
+    aparece.style.display="block";
 });
+
+button.addEventListener("mouseout", () =>{
+    aparece.style.display="none";
+});
+
+//queremos empezar a retirar eventos, vamos a meter una parte de código en una función
+
+const buttonClickCallback = () =>{
+    alert("Baruch HaShem");
+};
+
+button.addEventListener("click",buttonClickCallback );
+
+//los navegadores vienen con un setime
+
+setTimeout(() =>{
+    button.removeEventListener("click",buttonClickCallback);}, 2000 );
